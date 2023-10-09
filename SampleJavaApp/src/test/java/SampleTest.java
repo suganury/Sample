bd5d846ce8616a002514a7040dbc5b106c186c02
suganury <r-suganuma@outlook.com>
2023-10-09 23:05:16 +0900

    sampleコードコミット

---
+++ SampleJavaApp/src/test/java/SampleTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+	public class TestTest {
+
+		@Test
+		public void successTest() {
+			assertTrue(true);
+		}
+
+		//@Test
+		public void failureTest() {
+			assertTrue(false);
+		}
+	}
+
+}
